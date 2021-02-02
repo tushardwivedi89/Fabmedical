f6e7452d0030fd0a385b7b8419df1cf29909bca6
paramesh001 <parameswar.moole@gmail.com>
2021-02-02 05:37:06 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("4a180bec-e24f-4053-a528-5c58ce555527");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
